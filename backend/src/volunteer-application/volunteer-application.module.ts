22c9b002ad225e4b52cdd646e03103f5dd4cc43a
helina8b <helinabeneberu@visionvertex.tech>
2025-08-27 08:39:54 +0300

    modified volunteer application story 1

--- backend/src/volunteer-application/volunteer-application.module.ts
+++ backend/src/volunteer-application/volunteer-application.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { VolunteerApplicationService } from './volunteer-application.service';
 import { VolunteerApplicationController } from './volunteer-application.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [VolunteerApplicationController],
   providers: [VolunteerApplicationService],
   exports: [VolunteerApplicationService]
